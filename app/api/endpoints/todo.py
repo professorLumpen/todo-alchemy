e02c182c46a033251bec720b3f8a77384b6a3da4
professorLumpen <jeanpaulvsratyi@gmail.com>
2025-06-17 11:45:01 +0300

    Added db_repository for todo

--- app/api/endpoints/todo.py
+++ app/api/endpoints/todo.py
@@ -1,25 +1,23 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from app.api.schemas.todo import ToDoCreate, ToDoFromDB
 from app.db.database import get_db
-from app.db.models import ToDo
+from app.repositories.todo_repository import SQLAlchemyToDoRepository, ToDoRepository
 
 
 todo_router = APIRouter(prefix="/todo", tags=["ToDo"])
 
 
+async def get_todo_repository(session: AsyncSession = Depends(get_db)):
+    return SQLAlchemyToDoRepository(session)
+
+
 @todo_router.get("/", response_model=list[ToDoFromDB])
-async def get_todos(session: AsyncSession = Depends(get_db)):
-    result = await session.execute(select(ToDo))
-    return result.scalars().all()
+async def get_todos(todo_repo: ToDoRepository = Depends(get_todo_repository)):
+    return await todo_repo.get_todos()
 
 
 @todo_router.post("/", response_model=ToDoFromDB)
-async def create_todo(todo: ToDoCreate, session: AsyncSession = Depends(get_db)):
-    todo_db = ToDo(**todo.model_dump())
-    session.add(todo_db)
-    await session.commit()
-    await session.refresh(todo_db)
-    return todo_db
+async def create_todo(todo: ToDoCreate, todo_repo: ToDoRepository = Depends(get_todo_repository)):
+    return await todo_repo.create_todo(todo)
